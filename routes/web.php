7b75be789c33cc344b8f7fde4f5f6f618c750bf3
Aymane Benhima <aymanebenhima@gmail.com>
2019-12-28 17:23:38 +0100

    Configure Middleware \n home \t routes \t posts

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+
+    Route::get('/home', [
+        'uses' => 'HomeController@index',
+        'as'   => 'home'
+    ]);
 
     Route::get('/post/create', [
         'uses' => 'PostsController@create',
