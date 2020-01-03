94880247c91b4df0efd942f456b5fb07a811cd52
Aymane Benhima <aymanebenhima@gmail.com>
2020-01-03 15:31:45 +0100

    add middleware using auth class and user method

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::user()->admin) {
+
+            Session::flash('info', 'You do not have permissions to perfom this action.');
+
+            return redirect()->back();
+        }
+        
+        return $next($request);
+    }
+}
